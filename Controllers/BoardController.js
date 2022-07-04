24f2d32d95ff0c2cc13fcf9085a28c131a929c7a
ItayMayo <itaymayo02@gmail.com>
2022-07-04 12:27:54 +0300

    Controllers: BoardController - Added onBoardGenerated callback to link between the Board model and isBoardSolvable() method of GameProcessor.

--- Controllers/BoardController.js
+++ Controllers/BoardController.js
@@ -1,12 +1,23 @@
 export class BoardController {
-    constructor (model, view)
+    #_gameController;
+
+    constructor (model, view, gameController)
     {
         this.Model = model;
         this.View = view;
+        this.#_gameController = gameController;
 
         this.Model.bindBoardChanged(this.onBoardChanged);
+        this.Model.bindOnBoardGenerated(this.onBoardGenerated);
         this.Model.updateBoard();
         this.View.bindTileClick(this.onTileClick);
+    }
+
+    onBoardGenerated = board =>
+    {
+        const isSolvable = this.#_gameController.isBoardSolvable(board);
+
+        return isSolvable;
     }
 
     onBoardChanged = board => 
