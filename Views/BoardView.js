c06fdd705bb54fd3e4bc50720c53b4b7f03d550a
ItayMayo <itaymayo02@gmail.com>
2022-07-04 20:27:56 +0300

    Views: BoardView - #root => #main. Added a field that displays the user information. Added Reset Board button and Change User button.

--- Views/BoardView.js
+++ Views/BoardView.js
@@ -4,11 +4,16 @@ export class BoardView {
     #_boardContainer;
     #_gameOverTitle;
     #_clickHandler;
+    #_onResetButtonClick;
+    #_onChangeUserButtonClick;
+    #_user;
 
-    constructor(elementOperations)
+    constructor(elementOperations, user)
     {
+        this.#_user = user;
         this.#_elementOperations = elementOperations;
-        this.#_rootElement = this.#_elementOperations.getElement("#root");
+        this.#_rootElement = this.#_elementOperations.getElement("#main");
+        this.clearRoot();
         this.#_boardContainer = this.#_elementOperations.createElement("div", "boardContainer");
         this.#_clickHandler = undefined;
         const gameTitle = this.#_elementOperations.createElement("h1");
@@ -17,6 +22,43 @@ export class BoardView {
         this.#_gameOverTitle = this.#_elementOperations.createElement("h2");
         this.#_gameOverTitle.textContent = ""
         this.#_rootElement.append(this.#_gameOverTitle);
+        this.displayUserInfo();
+        this.displayResetBoardButton();
+        this.displayChangeUserButton();
+    }
+
+    clearRoot()
+    {
+        while (this.#_rootElement.firstChild)
+        {
+            const firstChild = this.#_rootElement.firstChild;
+            this.#_rootElement.removeChild(firstChild);
+        }
+    }
+
+    displayUserInfo()
+    {
+        const userInfoContainer = this.#_elementOperations.createElement("div", "user-info-container");
+        const userInfoLabel = this.#_elementOperations.createElement("h4", "user-info-text");
+        userInfoLabel.textContent = `Name: ${this.#_user.Name} Rank: ${this.#_user.Rank}`;
+        userInfoContainer.append(userInfoLabel);
+        this.#_rootElement.append(userInfoContainer);
+    }
+
+    displayResetBoardButton()
+    {
+        const resetBoardButton = this.#_elementOperations.createElement("button", "reset-button");
+        resetBoardButton.textContent = "Reset Board";
+        resetBoardButton.addEventListener("click", () => this.#_onResetButtonClick());
+        this.#_rootElement.append(resetBoardButton);
+    }
+
+    displayChangeUserButton() 
+    {
+        const changeUserButton = this.#_elementOperations.createElement("button", "change-user-button");
+        changeUserButton.textContent = "Change User";
+        changeUserButton.addEventListener("click", () => this.#_onChangeUserButtonClick());
+        this.#_rootElement.append(changeUserButton);
     }
 
     displayBoard(gameBoard) 
@@ -43,6 +85,16 @@ export class BoardView {
     bindTileClick(handler)
     {
         this.#_clickHandler = handler;
+    }
+
+    bindResetButtonClick(handler)
+    {
+        this.#_onResetButtonClick = handler;
+    }
+
+    bindChangeUserButtonClick(handler)
+    {
+        this.#_onChangeUserButtonClick = handler;
     }
 
     onGameOver()
