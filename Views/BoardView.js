254e76b5b17bdf42a5ff578ad93a214c6dc13bd1
ItayMayo <itaymayo02@gmail.com>
2022-07-04 10:22:51 +0300

    Views: Added BoardView.js to present the board to the user and listen for input.

---
+++ Views/BoardView.js
@@ -0,0 +1,46 @@
+import {ElementOperations} from "../Core/ElementsOperations/ElementOperations.js"
+
+export class BoardView {
+    #_elementOperations;
+    #_rootElement;
+    #_boardContainer;
+    #_clickHandler;
+
+    constructor() 
+    {
+        this.#_elementOperations = new ElementOperations();
+        this.#_rootElement = this.#_elementOperations.getElement("#root");
+        this.#_boardContainer = this.#_elementOperations.createElement("div", "boardContainer");
+        this.#_clickHandler = undefined;
+        const title = this.#_elementOperations.createElement("h1");
+        title.textContent = "GetSchwifty"
+        this.#_rootElement.append(title, this.#_boardContainer);
+    }
+
+    displayBoard(board) 
+    {
+        while (this.#_boardContainer.firstChild)
+        {
+            const firstChild = this.#_boardContainer.firstChild;
+            this.#_boardContainer.removeChild(firstChild);
+        }
+
+        this.#_boardContainer.style.gridTemplateColumns = `repeat(${board.length}, 1fr)`;
+
+        board.forEach(row => 
+        {
+            row.forEach(tile => 
+            {
+                let tileButton = this.#_elementOperations.createElement("button", "tile-button");
+                tileButton.textContent = tile.Value;
+                tileButton.addEventListener("click", () => this.#_clickHandler(tile));
+                this.#_boardContainer.append(tileButton);
+            });
+        });
+    }
+
+    bindTileClick(handler)
+    {
+        this.#_clickHandler = handler;
+    }
+}
