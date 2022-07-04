7acaef57c91ea24d2db285608f896236bfaa728a
ItayMayo <itaymayo02@gmail.com>
2022-07-04 12:28:25 +0300

    Bootstrapper - Initiated GameController instance for use in BoardController.

--- Bootstrapper.js
+++ Bootstrapper.js
@@ -1,9 +1,10 @@
 import { BoardView } from "./Views/BoardView.js";
 import { Board } from "./Models/Board.js";
 import { BoardController } from "./Controllers/BoardController.js";
+import { GameController } from "./Processors/GameProcessor.js";
 
 const board = new Board();
 const boardView = new BoardView();
+const gameController = new GameController();
 
-const boardController = new BoardController(board, boardView);
-//board.resetBoard();
+const boardController = new BoardController(board, boardView, gameController);
