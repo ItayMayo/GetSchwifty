80d1c8012f5c4fbf5273a9d374ffc0411b67e2ba
ItayMayo <itaymayo02@gmail.com>
2022-07-04 20:22:31 +0300

    Models: Added TableEntry model with required table information.

---
+++ Models/TableEntry.js
@@ -0,0 +1,11 @@
+export class TableEntry
+{
+    constructor(name, rank, gameLength, boardSize, startingDate)
+    {
+        this.Name = name;
+        this.Rank = rank;
+        this.GameLength = gameLength;
+        this.BoardSize = boardSize;
+        this.StartingDate = startingDate;
+    }
+}
