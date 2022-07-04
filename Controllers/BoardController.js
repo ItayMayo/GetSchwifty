10d2a503a9d0969ae15067253c1c4ddec0ca6be7
ItayMayo <itaymayo02@gmail.com>
2022-07-04 15:51:36 +0300

    Controllers: BoardController - Code cleanup.

--- Controllers/BoardController.js
+++ Controllers/BoardController.js
@@ -6,7 +6,6 @@ export class BoardController {
         this.Model = model;
         this.View = view;
         this.#_gameController = gameController;
-
         this.Model.bindBoardChanged(this.onBoardChanged);
         this.Model.bindOnBoardGenerated(this.onBoardGenerated);
         this.Model.updateBoard();
