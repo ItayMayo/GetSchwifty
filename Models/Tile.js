9780b685a376d56e1a327a58c6f88c834d7a5b9e
ItayMayo <itaymayo02@gmail.com>
2022-07-04 10:21:41 +0300

    Models: Added Tile.js to store tile-related information such as value and location on board.

---
+++ Models/Tile.js
@@ -0,0 +1,9 @@
+export class Tile {
+    constructor(value, row, column, onClick) 
+    {
+        this.Value = value;
+        this.Row = row;
+        this.Column = column;
+        this.OnClick = onClick;
+    }
+}
