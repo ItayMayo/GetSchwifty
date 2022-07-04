477865dd02b5763b80f46dd5dc5c76d1e806c202
ItayMayo <itaymayo02@gmail.com>
2022-07-04 15:50:55 +0300

    Bootstrapper - Added code to initiate TileOperations.

--- Bootstrapper.js
+++ Bootstrapper.js
@@ -3,9 +3,13 @@ import { Board } from "./Models/Board.js";
 import { BoardController } from "./Controllers/BoardController.js";
 import { GameController } from "./libs/Processors/GameProcessor.js";
 import { BasicWinStrategy } from "./libs/WinStrategies/BasicWinStrategy.js";
+import { ElementOperations } from "./libs/ElementsOperations/ElementOperations.js";
+import { TileOperations } from "./libs/TilesOperations/TileOperations.js";
 
-const board = new Board();
-const boardView = new BoardView();
+const tileOperations = new TileOperations();
+const board = new Board(tileOperations);
+const elementOperations = new ElementOperations();
+const boardView = new BoardView(elementOperations);
 const winStrategy = new BasicWinStrategy();
 const gameController = new GameController(winStrategy);
 
